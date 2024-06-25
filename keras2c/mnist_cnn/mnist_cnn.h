7a1846817160cfab1315a90fc78ae24c59a458a2
SoukaryaETCE <143642544+SoukaryaETCE@users.noreply.github.com>
2024-06-25 18:13:51 +0530

    Add files via upload

---
+++ keras2c/mnist_cnn/mnist_cnn.h
@@ -0,0 +1,5 @@
+#pragma once 
+#include "./include/k2c_tensor_include.h" 
+void mnist_cnn(k2c_tensor* input_1_input, k2c_tensor* dense_output); 
+void mnist_cnn_initialize(); 
+void mnist_cnn_terminate(); 
