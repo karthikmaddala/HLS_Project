7ee1846a6b7becb7dd8835a95ab94caa61f2289d
SoukaryaETCE <143642544+SoukaryaETCE@users.noreply.github.com>
2024-06-25 17:43:14 +0530

    Delete brain_tumor.h

--- brain_tumor.h
+++
@@ -1,5 +0,0 @@
-#pragma once 
-#include "./include/k2c_tensor_include.h" 
-void brain_tumor(k2c_tensor* input_9_input, k2c_tensor* dense_32_output); 
-void brain_tumor_initialize(); 
-void brain_tumor_terminate(); 
