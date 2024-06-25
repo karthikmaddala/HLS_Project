fd026e6f257ed66b7fd0fd85ce300eaba9eb8782
SoukaryaETCE <143642544+SoukaryaETCE@users.noreply.github.com>
2024-06-25 18:06:18 +0530

    Add files via upload

---
+++ keras2c/music_genre_CNN/music.h
@@ -0,0 +1,5 @@
+#pragma once 
+#include "./include/k2c_tensor_include.h" 
+void music(k2c_tensor* conv2d_4_input_input, k2c_tensor* dense_3_output); 
+void music_initialize(); 
+void music_terminate(); 
