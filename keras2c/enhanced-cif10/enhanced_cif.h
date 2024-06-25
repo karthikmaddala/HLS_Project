913451a2534aad2d3097e80c79f631e72f033e5f
SoukaryaETCE <143642544+SoukaryaETCE@users.noreply.github.com>
2024-06-25 18:09:02 +0530

    Add files via upload

---
+++ keras2c/enhanced-cif10/enhanced_cif.h
@@ -0,0 +1,5 @@
+#pragma once 
+#include "./include/k2c_tensor_include.h" 
+void enhanced_cif(k2c_tensor* conv2d_input_input, k2c_tensor* dense_1_output); 
+void enhanced_cif_initialize(); 
+void enhanced_cif_terminate(); 
