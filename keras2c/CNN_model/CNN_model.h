cdf1bef2ebb8da621bbf58e7e0ad10db47b1b31e
SoukaryaETCE <143642544+SoukaryaETCE@users.noreply.github.com>
2024-06-25 18:01:44 +0530

    Add files via upload

---
+++ keras2c/CNN_model/CNN_model.h
@@ -0,0 +1,5 @@
+#pragma once 
+#include "./include/k2c_tensor_include.h" 
+void CNN_model(k2c_tensor* conv2d_input_input, k2c_tensor* dense_2_output); 
+void CNN_model_initialize(); 
+void CNN_model_terminate(); 
