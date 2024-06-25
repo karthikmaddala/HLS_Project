7431d53d6ebe984ffeb0af4fae69b3c1c84fc552
SoukaryaETCE <143642544+SoukaryaETCE@users.noreply.github.com>
2024-06-25 18:11:44 +0530

    Add files via upload

---
+++ keras2c/har_cnn/har_cnn.h
@@ -0,0 +1,5 @@
+#pragma once 
+#include "./include/k2c_tensor_include.h" 
+void har_cnn(k2c_tensor* conv2d_1_input_input, k2c_tensor* dense_3_output); 
+void har_cnn_initialize(); 
+void har_cnn_terminate(); 
