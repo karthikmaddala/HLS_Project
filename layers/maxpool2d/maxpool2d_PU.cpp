e24051ed64f224fcc7d4ec51836a40a7ed37475e
Sutirtha Bhattacharyya <sutirtha8623@gmail.com>
2024-06-28 19:21:59 +0530

    Update maxpool2d_PU.cpp

--- layers/maxpool2d/maxpool2d_PU.cpp
+++ layers/maxpool2d/maxpool2d_PU.cpp
@@ -14,28 +14,29 @@ void k2c_maxpool2d(float output_array[2704],
                 const size_t pool_size[2],
                 const size_t stride[2]) {
 
-
     const size_t channels = input_shape[2];
-    // i,j,l output indices
-    /// i, k, m input indices
-    for (size_t i=0; i< channels; ++i) {
-#pragma HLS loop_tripcount min=26 max=26
-        for (size_t j=0, k=0; j<output_shape[1]*channels; j+=channels, k+=channels*stride[1]) {
-#pragma HLS loop_tripcount min=13 max=13
-            for (size_t l=0, m=0; l<output_numel; l+=channels*output_shape[1], m+=channels*input_shape[1]*stride[0]) {
-#pragma HLS loop_tripcount min=8 max=8
-                output_array[l+j+i] = input_array[m+k+i];
-                for (size_t n=0; n<pool_size[1]*channels; n+=channels) {
+
+    // Main loops with unrolling and pipelining
+    for (size_t i = 0; i < channels; ++i) {
+        #pragma HLS loop_tripcount min=26 max=26
+        for (size_t j = 0, k = 0; j < output_shape[1] * channels; j += channels, k += channels * stride[1]) {
+            #pragma HLS loop_tripcount min=13 max=13
+            for (size_t l = 0, m = 0; l < output_numel; l += channels * output_shape[1], m += channels * input_shape[1] * stride[0]) {
+                #pragma HLS loop_tripcount min=8 max=8
+                #pragma HLS PIPELINE II=1
+                float max_val = input_array[m + k + i];
+                for (size_t n = 0; n < pool_size[1] * channels; n += channels) {
 #pragma HLS loop_tripcount min=2 max=2
-#pragma HLS pipeline II=1
-                    for (size_t p=0; p<pool_size[0]*channels*input_shape[1]; p+=channels*input_shape[1]) {
+                    #pragma HLS UNROLL
+                    for (size_t p = 0; p < pool_size[0] * channels * input_shape[1]; p += channels * input_shape[1]) {
 #pragma HLS loop_tripcount min=2 max=2
-#pragma HLS unroll
-                        if (output_array[l+j+i] < input_array[m+k+i+n+p]) {
-                            output_array[l+j+i] = input_array[m+k+i+n+p];
+                        #pragma HLS UNROLL
+                        if (max_val < input_array[m + k + i + n + p]) {
+                            max_val = input_array[m + k + i + n + p];
                         }
                     }
                 }
+                output_array[l + j + i] = max_val;
             }
         }
     }
