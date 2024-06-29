2e93a790a13b47bedd8df20297ca78526344977b
SoukaryaETCE <143642544+SoukaryaETCE@users.noreply.github.com>
2024-06-29 18:45:24 +0530

    Create tanh_PU2.cpp

---
+++ layers/tanh/tanh_PU2.cpp
@@ -0,0 +1,15 @@
+#include <stddef.h>
+#include <math.h>
+#include <stddef.h>
+#include "tanh.h"
+
+void k2c_tanh_func(float x[10000], const size_t size) {
+
+
+    for (size_t i=0; i<size; ++i) {
+        #pragma HLS loop_tripcount min=10000 max=10000
+#pragma HLS PIPELINE II=1
+#pragma HLS UNROLL factor=2
+        x[i] = tanhf(x[i]);
+    }
+}
