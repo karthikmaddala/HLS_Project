61b725f12c7c83bed3fb0b895fa422291489e91f
Sutirtha Bhattacharyya <sutirtha8623@gmail.com>
2024-06-29 15:59:23 +0530

    Create softmax_Pac4PM.cpp

---
+++ layers/softmax/softmax_Pac4PM.cpp
@@ -0,0 +1,38 @@
+#include <stddef.h>
+#include <math.h>
+#include "softmax.h"
+
+void k2c_softmax_func(float x[10000], const size_t size) {
+
+    float xmax = x[0];
+    float sum = 0;
+#pragma HLS ARRAY_PARTITION variable=x block factor=4
+    // Find xmax with pipelining and unrolling
+    for (size_t i = 0; i < size; ++i) {
+        #pragma HLS loop_tripcount min=10000 max=10000
+        #pragma HLS PIPELINE II=1
+
+        if (x[i] > xmax) {
+            xmax = x[i];
+        }
+    }
+
+    // Compute exponentials and partial sum with pipelining and unrolling
+    for (size_t i = 0; i < size; ++i) {
+        #pragma HLS loop_tripcount min=10000 max=10000
+        #pragma HLS PIPELINE II=1
+
+        x[i] = expf(x[i] - xmax);
+        sum += x[i];
+    }
+
+    sum = 1.0f / sum;
+
+    // Normalize with pipelining and unrolling
+    for (size_t i = 0; i < size; ++i) {
+        #pragma HLS loop_tripcount min=10000 max=10000
+        #pragma HLS PIPELINE II=1
+
+        x[i] = x[i] * sum;
+    }
+}
