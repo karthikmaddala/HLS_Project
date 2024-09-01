47a7d87df1812040ecf9703aa44ff97ddfb2ecc2
Fedrick Nongpoh <75745941+rick-2001@users.noreply.github.com>
2024-09-01 15:18:49 +0530

    Add files via upload

---
+++ CNN_HLS/baseline/CNN_model.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <stdlib.h>
+
+
+/**
+ * Rank of largest keras2c tensors.
+ * mostly used to ensure a standard size for the tensor.shape array.
+ */
+#define K2C_MAX_NDIM 5
+#define MAX_ARRAY_SIZE 10000
+
+
+/**
+ * tensor type for keras2c.
+ */
+struct k2c_tensor
+{
+    /** Rank of the tensor (number of dimensions). */
+    size_t ndim;
+
+    /** Number of elements in the tensor. */
+    size_t numel;
+
+    /** Array, size of the tensor in each dimension. */
+    size_t shape[K2C_MAX_NDIM];
+};
+
+
+typedef struct k2c_tensor_wa
+{
+    float array[MAX_ARRAY_SIZE];
+    size_t ndim;
+    size_t numel;
+    size_t shape[K2C_MAX_NDIM];
+}k2c_tensor_wa;
+
+typedef struct k2c_tensor k2c_tensor;
+
+
+void CNN_model(k2c_tensor_wa* conv2d_input_input, k2c_tensor_wa* dense_2_output);
+void CNN_model_initialize(); 
+void CNN_model_terminate(); 
