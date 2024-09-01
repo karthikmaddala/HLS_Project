f2f5f8b69b1b2383ec74db1507b46796c392cd89
Fedrick Nongpoh <75745941+rick-2001@users.noreply.github.com>
2024-09-01 15:32:17 +0530

    Add files via upload

---
+++ Covid19_HLS/finalcovid.h
@@ -0,0 +1,5 @@
+#pragma once 
+#include "./include/k2c_tensor_include.h" 
+void finalcovid(k2c_tensor* input_11_input, k2c_tensor* dense_31_output); 
+void finalcovid_initialize(); 
+void finalcovid_terminate(); 
