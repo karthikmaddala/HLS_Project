1166423c1e2e313b7f3d62f3cce6dee91106b184
Fedrick Nongpoh <75745941+rick-2001@users.noreply.github.com>
2024-09-27 15:38:02 +0530

    Add files via upload

---
+++ BrainTumor_LLM/finalbraintumor.h
@@ -0,0 +1,5 @@
+#pragma once 
+#include "./include/k2c_tensor_include.h" 
+void finalbraintumor(k2c_tensor* input_9_input, k2c_tensor* dense_32_output); 
+void finalbraintumor_initialize(); 
+void finalbraintumor_terminate(); 
