eefbab883d5562cf76dffd9b94e4d5f50ad2d79d
Fedrick Nongpoh <75745941+rick-2001@users.noreply.github.com>
2025-01-23 01:19:30 +0530

    Add files via upload

---
+++ MUSIC_CNN_LLM/baseline/music.h
@@ -0,0 +1,5 @@
+#pragma once 
+#include "./include/k2c_tensor_include.h" 
+void finalmusic1(k2c_tensor* conv2d_4_input_input, k2c_tensor* dense_3_output); 
+void finalmusic1_initialize(); 
+void finalmusic1_terminate(); 
